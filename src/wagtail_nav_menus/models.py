2565e7a6663a1d8ae981a6ab0772ca09587e5bd7
David Burke <david@burkesoftware.com>
2019-11-11 17:23:37 -0500

    lint

--- src/wagtail_nav_menus/models.py
+++ src/wagtail_nav_menus/models.py
@@ -114,6 +114,7 @@ class NavCategoryBlock(blocks.StructBlock):
 def site_default():
     return Site.objects.filter(is_default_site=True).first()
 
+
 def site_default_id():
     """ Not sure why this is needed for migrations, there is probably a better way. """
     site = site_default()
