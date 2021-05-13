10ea71d52860e39e11f0b3b937528358986de3e2
David Burke <david@burkesoftware.com>
2021-05-13 15:36:10 -0400

    Add wagtail 2.13 support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from versiontag import get_version, cache_git_tag  # NOQA
 packages = find_packages('src')
 
 install_requires = [
-    'wagtail>=2.7,<2.13',
+    'wagtail>=2.7,<2.14',
 ]
 extras_require = {
     'development': [
